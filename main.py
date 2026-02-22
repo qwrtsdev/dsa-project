e3aa4f7ba7fe2928255cc3c8dae7332a3bdb0ffa
qwrtsdev <potatoloadzch@gmail.com>
2026-02-22 22:48:34 +0700

    added simple commands

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ def main():
     match user_input:
       case "show":
         print(data)
+      case "get_max_load":
+        # TODO: Instantly returns the course with the highest credits.
+        pass
+      case "list_sorted":
+        # TODO: Prints all courses sorted by CourseCode
+        pass
       case "exit":
         sys.exit(0)
       case _:
@@ -28,6 +34,7 @@ def load_csv():
     reader = csv.reader(file)
     for row in reader:
       data.append(row)
+    data.pop(0)
 
 if __name__ == "__main__":
   main()
