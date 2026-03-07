533d701da0bfab2f7a309f99e8928758a76629d0
qwrtsdev <potatoloadzch@gmail.com>
2026-03-07 15:44:14 +0700

    starter code rework

--- main.py
+++ main.py
@@ -4,25 +4,19 @@ import sys
 data = []
 
 def main():
-  print("====================================")
-  print("Loading CSV file...")
   load_csv()
-  print("CSV file loaded successfully.")
-  print("====================================\n")
   options_selector()
 
 def options_selector():
   while True:
     user_input = input(">>> ")
-    
-    print("\n============== OUTPUT ============\n")
   
     match user_input:
-      case "show":
+      case "raw":
         print(data)
       case "get_max_load":
         # TODO: Instantly returns the course with the highest credits.
-        pass
+        get_max_load()
       case "list_sorted":
         # TODO: Prints all courses sorted by CourseCode
         list_sorted()
@@ -30,8 +24,6 @@ def options_selector():
         sys.exit(0)
       case _:
         print("Unknown command.")
-        
-    print("\n====================================\n")
   
 def load_csv():
   with open("CprE_Subject.csv", "r") as file:
@@ -40,10 +32,11 @@ def load_csv():
       data.append(row)
     data.pop(0)
     
+def get_max_load():
+  pass
+    
 def list_sorted():
-  sorted_data = sorted(data, key=lambda x: x[0])  # Sort by CourseCode
-  for course in sorted_data:
-    print(f"{course[0]} - {course[1]}")
+  pass
 
 if __name__ == "__main__":
   main()
