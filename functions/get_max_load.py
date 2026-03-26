a2206baea4c1bf9387b3568a330d5626d2925726
qwrtsdev <potatoloadzch@gmail.com>
2026-03-27 03:29:06 +0700

    get_max_load completed

--- functions/get_max_load.py
+++ functions/get_max_load.py
@@ -1,22 +1,42 @@
 def get_max_load(data):
-  max_value = 0
-  results = []
+  if len(data) == 0:
+    return False
+  elif len(data) == 1:
+    print(f"{data[0][0]} {data[0][1]} {data[0][2]} {data[0][3]} {data[0][4]} {data[0][5]}")
+  else:
+    heap = [(int(row[3].split("(")[0]), row) for row in data]
 
-  # searching the maximum credit value
-  for item in data:
-    split_data = item[3].split("(")
-    current_credit_value = int(split_data[0])
+    heap_length = len(heap)
+    start = (heap_length - 1) // 2
+
+    for parent_pos in range(start, -1, -1):
+      swap_heap(heap, parent_pos, heap_length)
+
+    max_credit = heap[0][0]
     
-    if current_credit_value > max_value:
-      max_value = current_credit_value
-  
-  # appending a list of subjects based on maximum value
-  for item in data:
-    split_data = item[3].split("(")
-    current_credit_value = int(split_data[0])
-    
-    if current_credit_value == max_value:
-      results.append(item)
-      
-  for item in results:
-    print(f"{item[0]} {item[1]} {item[2]} {item[3]} {item[4]} {item[5]}")
+    results = []
+    for credit, row in heap:
+      if credit == max_credit:
+        results.append(row)
+
+    for item in results:
+      print(f"{item[0]} {item[1]} {item[2]} {item[3]} {item[4]} {item[5]}")
+
+
+def swap_heap(heap, parent_pos, heap_length):
+  while True:
+    larger = parent_pos
+    left    = 2 * parent_pos + 1
+    right   = 2 * parent_pos + 2
+
+    if left < heap_length and heap[left][0] > heap[larger][0]:
+      larger = left
+
+    if right < heap_length and heap[right][0] > heap[larger][0]:
+      larger = right
+
+    if larger == parent_pos:
+      break
+
+    heap[parent_pos], heap[larger] = heap[larger], heap[parent_pos]
+    parent_pos = larger
